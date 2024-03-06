cc92f77d54ea50928aa00999d9392905ef84b5ac
carresemata <carrese@rvohealth.com>
2024-03-06 10:58:21 -0400

    update base.address

--- migration/ods1_stage/tables/base/address/spu_translated_address.sql
+++ migration/ods1_stage/tables/base/address/spu_translated_address.sql
@@ -1,96 +1,117 @@
---- 1. etl_spuMergeOfficeAddress (snowflake db)
+--------------- 1. etl_spuMergeOfficeAddress (snowflake db)
 
--- CTE 'w' retrieves records from FacilityProfileProcessing and FacilityProfileProcessingDeDup tables
--- It also extracts 'Address' from 'EntityJSONString' and assigns it to the 'FacilityJSON' column
-WITH cte_w AS (
+-- First CTE: Extract 'Address' from 'EntityJSONString' in the tables FacilityProfileProcessing and FacilityProfileProcessingDeDup
+WITH cte_facility AS (
     SELECT 
-        p.CREATE_DATE, 
-        p.RELTIO_ID as ReltioEntityID, 
-        p.Facility_CODE as FacilityCode, 
-        p.FacilityID,
-        PARSE_JSON(p.PAYLOAD:EntityJSONString.Address) AS FacilityJSON  -- Extract 'Address' from 'EntityJSONString' !!!!!!!!!!!!!!!!! THIS MIGHT BE INCORRECT
-    FROM raw.FacilityProfileProcessingDeDup AS d
-    JOIN raw.FacilityProfileProcessing AS p ON p.rawFacilityProfileID = d.rawFacilityProfileID
-    WHERE p.PAYLOAD IS NOT NULL
+        p.create_date, 
+        p.reltio_id AS reltio_entity_id, 
+        p.facility_code AS facility_code, 
+        p.facility_id,
+        PARSE_JSON(p.payload:EntityJSONString.Address) AS facility_json -- The parse_json might not be needed
+    FROM raw.facility_profile_processing_de_dup AS d
+    JOIN raw.facility_profile_processing AS p ON p.raw_facility_profile_id = d.raw_facility_profile_id
+    WHERE p.payload IS NOT NULL
 ),
 
--- CTE 'x' retrieves all records from 'w' where 'FacilityJSON' is not NULL
-cte_x AS (
-    SELECT *
-    FROM cte_w
-    WHERE cte_w.FacilityJSON IS NOT NULL
-),
-
-
--- CTE 'y' parses JSON fields from 'FacilityJSON' and assigns them to SQL fields
--- It also calculates the row number for each 'FacilityID' ordered by 'CREATE_DATE' in descending order
-cte_y AS (
+-- Second CTE: Filter out records where 'FacilityJSON' is not null
+cte_payload AS (
     SELECT 
-        cte_x.FacilityID,
-        cte_x.FacilityJSON:AddressLine1::VARCHAR(150) AS AddressLine1,  -- Parse 'AddressLine1' from 'FacilityJSON'
-        cte_x.FacilityJSON:City::VARCHAR(150) AS City,  -- Parse 'City' from 'FacilityJSON'
-        cte_x.FacilityJSON:StateProvince::VARCHAR(50) AS State,  -- Parse 'StateProvince' from 'FacilityJSON'
-        cte_x.FacilityJSON:Country::VARCHAR(50) AS Country,  -- Parse 'Country' from 'FacilityJSON'
-        cte_x.FacilityJSON:PostalCode::VARCHAR(50) AS PostalCode,  -- Parse 'PostalCode' from 'FacilityJSON'
-        cte_x.FacilityJSON:Latitude::VARCHAR(50) AS Latitude,  -- Parse 'Latitude' from 'FacilityJSON'
-        cte_x.FacilityJSON:Longitude::VARCHAR(50) AS Longitude,  -- Parse 'Longitude' from 'FacilityJSON'
-        cte_x.FacilityJSON:LastUpdateDate::DATETIME AS LastUpdateDate,  -- Parse 'LastUpdateDate' from 'FacilityJSON'
-        cte_x.FacilityJSON:SourceCode::VARCHAR(80) AS SourceCode,  -- Parse 'SourceCode' from 'FacilityJSON'
-        cte_x.FacilityCode,
-        ROW_NUMBER() OVER(PARTITION BY cte_x.FacilityID ORDER BY cte_x.CREATE_DATE DESC) AS RowRank  -- Calculate row number
-    FROM cte_x
+        f.facility_id,
+        f.facility_code,
+        f.facility_json,
+        f.create_date
+    FROM cte_facility AS f
+    WHERE cte_facility.facility_json IS NOT NULL
 ),
---- create swimlane CTE
 
-cte_swimlane AS (
+-- Third CTE: Parse JSON fields from 'FacilityJSON' and assign them to SQL fields
+cte_parse_address AS (
+    SELECT 
+        cte_payload.facility_id,
+        cte_payload.facility_json:AddressLine1::VARCHAR(150) AS address_line1,
+        cte_payload.facility_json:City::VARCHAR(150) AS city,
+        cte_payload.facility_json:StateProvince::VARCHAR(50) AS state,
+        cte_payload.facility_json:Country::VARCHAR(50) AS country,
+        cte_payload.facility_json:PostalCode::VARCHAR(50) AS postal_code,
+        cte_payload.facility_json:Latitude::VARCHAR(50) AS latitude,
+        cte_payload.facility_json:Longitude::VARCHAR(50) AS longitude,
+        cte_payload.facility_json:LastUpdateDate::DATETIME AS last_update_date,
+        cte_payload.facility_json:SourceCode::VARCHAR(80) AS source_code,
+        cte_payload.facility_code,
+        ROW_NUMBER() OVER(PARTITION BY cte_payload.facility_id ORDER BY cte_payload.create_date DESC) AS row_rank
+    FROM cte_payload
+),
+
+-- Fourth CTE: Create 'Swimlane' structure with distinct data
+cte_insert_address AS (
     SELECT
-        DISTINCT cte_y.FacilityID
-        ,cte_y.AddressLine1
-        ,cte_y.City
-        ,cte_y.State
-        ,cte_y.Country
-        ,cte_y.PostalCode
-        ,cte_y.Latitude
-        ,cte_y.Longitude
-        ,cte_y.SourceCode
-        ,cte_y.FacilityCode
-        ,ROW_NUMBER() OVER(PARTITION BY cte_y.FacilityID ORDER BY cte_y.CREATE_DATE DESC) AS RowRank
-    FROM cte_y )
+        cte_parse_address.facility_id,
+        cte_parse_address.address_line1,
+        cte_parse_address.city,
+        cte_parse_address.state,
+        cte_parse_address.country,
+        cte_parse_address.postal_code,
+        cte_parse_address.latitude,
+        cte_parse_address.longitude,
+        cte_parse_address.source_code,
+        cte_parse_address.facility_code,
+        ROW_NUMBER() OVER(PARTITION BY cte_parse_address.facility_id ORDER BY cte_parse_address.create_date DESC) AS row_rank
+    FROM cte_parse_address
+    GROUP BY 
+        cte_parse_address.facility_id, 
+        cte_parse_address.address_line1, 
+        cte_parse_address.city, 
+        cte_parse_address.state, 
+        cte_parse_address.country, 
+        cte_parse_address.postal_code, 
+        cte_parse_address.latitude, 
+        cte_parse_address.longitude, 
+        cte_parse_address.source_code, 
+        cte_parse_address.facility_code
+),
 
-
-------------  !!!!!!!!!!!!!!!!!! WE COULD REMOVE CTE_X
-
-
--- Create CTE to insert data into ODS1Stage.Base.Address table
-WITH cte_insert AS (
+-- Fifth CTE: Prepare data for insertion into 'ods1stage.base.address' table
+cte_insert AS (
         SELECT
-            DISTINCT S.AddressLine1, 
-            S.Latitude, 
-            S.Longitude, 
-            NULL AS refTimeZoneCode, --- !!! MAYBE THIS DOES NOT WORK
-            CSP.CityStatePostalCodeId
+            ia.address_line1, 
+            ia.latitude, 
+            ia.longitude, 
+            NULL AS ref_time_zone_code, 
+            csp.city_state_postal_code_id
         FROM
-            cte_swimlane as S
-        JOIN ODS1Stage.Base.CityStatePostalCode CSP ON CSP.City = S.City AND CSP.State = S.State AND CSP.PostalCode = S.PostalCode
-        LEFT JOIN ODS1Stage.Base.Address A ON A.AddressLine1 = S.AddressLine1 AND CSP.CityStatePostalCodeId = A.CityStatePostalCodeId
+            cte_insert_address AS ia
+        JOIN ods1stage.base.city_state_postal_code csp ON csp.city = ia.city AND csp.state = ia.state AND csp.postal_code = ia.postal_code
+        LEFT JOIN ods1stage.base.address a ON a.address_line1 = ia.address_line1 AND csp.city_state_postal_code_id = a.city_state_postal_code_id
         WHERE
-            A.AddressId IS NULL)
+            a.address_id IS NULL
+        GROUP BY
+            ia.address_line1, 
+            ia.latitude, 
+            ia.longitude, 
+            csp.city_state_postal_code_id
+)
 
--- Insert data into ODS1Stage.Base.Address table
-INSERT INTO ODS1Stage.Base.Address(AddressId, NationId, AddressLine1, Latitude, Longitude, TimeZone, CityStatePostalCodeId)
+-- Insert data into 'ods1stage.base.address' table
+INSERT INTO ods1stage.base.address(
+    address_id, 
+    nation_id, 
+    address_line1, 
+    latitude, 
+    longitude, 
+    time_zone, 
+    city_state_postal_code_id)
 SELECT
-    UUID_STRING(),  -- Equivalent o NewId() in Snowflake
+    UUID_STRING(),  -- Equivalent to NewId() in T-SQL
     '00415355-0000-0000-0000-000000000000', 
-    S.AddressLine1, 
-    S.Latitude, 
-    S.Longitude, 
-    refTimeZoneCode, 
-    S.CityStatePostalCodeId
+    i.address_line1, 
+    i.latitude, 
+    i.longitude, 
+    i.ref_time_zone_code, 
+    i.city_state_postal_code_id
 FROM
-    cte_insert S
+    cte_insert AS i;
 
-
--- Update AddressCode when it is null (THIS CAN BE ADDED WHEN CREATING THE TABLE DIRECTLY)
-UPDATE ODS1Stage.Base.Address
-SET AddressCode = 'AD' || RIGHT(TO_HEX(TO_BINARY(AddressInt)), 10)  -- CHECK IF THIS WORKS
-WHERE AddressCode IS NULL
+-- Update 'address_code' when it is null (THIS CAN BE ADDED WHEN CREATING THE TABLE DIRECTLY)
+UPDATE ods1stage.base.address
+SET address_code = 'AD' || RIGHT(TO_HEX(TO_BINARY(address_int)), 10)  -- Equivalent to original T-SQL statement
+WHERE address_code IS NULL;
