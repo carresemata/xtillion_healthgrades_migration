ed80f38df96b1df5ac945456803e8212e1e43e0a
carresemata <carrese@rvohealth.com>
2024-03-21 14:26:18 -0400

    updated sp_load_solrtreatmententrylevel

--- migration_original/ODS1Stage/tables/Show/SOLRTreatmentEntryLevel/spu_translated_SOLRTreatmentEntryLevel.sql
+++ migration_original/ODS1Stage/tables/Show/SOLRTreatmentEntryLevel/spu_translated_SOLRTreatmentEntryLevel.sql
@@ -1,6 +1,6 @@
 
 -- 1. spuSOLRTreatmentEntryLevel (validated in snowflake)
-CREATE OR REPLACE PROCEDURE DEV.SP_LOAD_SOLRTREATMENTENTRYLEVEL() 
+CREATE OR REPLACE PROCEDURE SHOW.SP_LOAD_SOLRTREATMENTENTRYLEVEL() 
     RETURNS STRING
     LANGUAGE SQL
     EXECUTE AS CALLER
@@ -19,16 +19,16 @@ DECLARE
 ---- Base.SpecialtyToProcedureMedical
 ---- Base.Specialty
 ---- Base.MedicalTerm
+    
 
 ---------------------------------------------------------
 --------------- 1. Declaring variables ------------------
 ---------------------------------------------------------
 
     select_statement STRING; -- CTE statement
-    load_statement STRING; -- Insert statement to final table
-    final_execution STRING; -- Execution of the load and select
+    insert_statement STRING; -- Insert statement to final table
+    merge_statement STRING; -- Merge statement
     status STRING; -- Status monitoring
-
    
 ---------------------------------------------------------
 --------------- 2.Conditionals if any -------------------
@@ -42,7 +42,7 @@ BEGIN
 --------------- 3. Select statements --------------------
 ---------------------------------------------------------     
 
-
+-- Select Statement
 select_statement:= 'WITH cte_treatment_spec AS (
                         SELECT
                             SpecCond.SpecialtyID,
@@ -63,7 +63,7 @@ select_statement:= 'WITH cte_treatment_spec AS (
                             JOIN Base.TreatmentLevel AS TreatLev ON TreatLev.TreatmentLevelID = SpecProc.TreatmentLevelID
                     )
                     
-                        SELECT
+                        SELECT DISTINCT
                             Med.RefMedicalTermCode AS DCPCode,
                             cteTreat.TreatmentLevelDescription,
                             Spec.SpecialtyCode,
@@ -72,35 +72,38 @@ select_statement:= 'WITH cte_treatment_spec AS (
                         FROM
                             cte_treatment_spec cteTreat
                             JOIN Base.Specialty Spec ON Spec.SpecialtyID = cteTreat.SpecialtyID
-                            JOIN Base.MedicalTerm Med ON Med.MedicalTermID = cteTreat.MedicalTermID
-                        GROUP BY
-                            DCPCode,
-                            cteTreat.TreatmentLevelDescription,
-                            Spec.SpecialtyCode,
-                            Spec.SpecialtyDescription,
-                            ForMarketViewLoad';
+                            JOIN Base.MedicalTerm Med ON Med.MedicalTermID = cteTreat.MedicalTermID';
+
+--- Insert Statement
+insert_statement := ' INSERT  (
+                        DCPCode,
+                        TreatmentLevelDescription,
+                        SpecialtyCode,
+                        SpecialtyDescription,
+                        ForMarketViewLoad)
+                      VALUES (
+                        source.DCPCode,
+                        source.TreatmentLevelDescription,
+                        source.SpecialtyCode,
+                        source.SpecialtyDescription,
+                        source.ForMarketViewLoad);';
 
                      
 ---------------------------------------------------------
 --------- 4. Actions (Inserts and Updates) --------------
 ---------------------------------------------------------  
 
-
-load_statement:= 'INSERT INTO Dev.SOLRTreatmentEntryLevel 
-                    (DCPCode,
-                    TreatmentLevelDescription,
-                    SpecialtyCode,
-                    SpecialtyDescription,
-                    ForMarketViewLoad)' ;
+merge_statement := ' MERGE INTO Show.SOLRTreatmentEntryLevel as target 
+                        USING (' ||select_statement|| ') as source 
+                        ON target.DCPCode = source.DCPCode AND target.SpecialtyCode = source.SpecialtyCode
+                        WHEN NOT MATCHED THEN '
+                            || insert_statement ;
 
 ---------------------------------------------------------
 ------------------- 5. Execution ------------------------
 --------------------------------------------------------- 
-                    
-final_execution:= load_statement || select_statement ;
-EXECUTE IMMEDIATE final_execution ;
 
-
+EXECUTE IMMEDIATE merge_statement ;
 
 ---------------------------------------------------------
 --------------- 6. Status monitoring --------------------
