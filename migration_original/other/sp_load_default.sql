e837ab4ef1bad4710d612294e8f89386498cbef8
carresemata <carrese@rvohealth.com>
2024-03-21 15:33:46 -0400

    default

---
+++ migration_original/other/sp_load_default.sql
@@ -0,0 +1,107 @@
+CREATE OR REPLACE PROCEDURE SP_LOAD_TABLE_NAME(ParameterName ParameterType) -- Parameters
+    RETURNS STRING
+    LANGUAGE SQL
+    EXECUTE AS CALLER
+    AS  
+
+DECLARE 
+
+---------------------------------------------------------
+--------------- 0. Table dependencies -------------------
+---------------------------------------------------------
+    
+-- #TABLE NAME# depends on: 
+--- #Table Dependency 1#
+--- #Table Dependency 2#
+--- #Table Dependency 3#
+
+---------------------------------------------------------
+--------------- 1. Declaring variables ------------------
+---------------------------------------------------------
+
+    select_statement STRING; -- CTE and Select statement for the Merge
+    update_statement STRING; -- Update statement for the Merge
+    insert_statement STRING; -- Insert statement for the Merge
+    merge_statement STRING; -- Merge statement to final table
+    status STRING; -- Status monitoring
+   
+---------------------------------------------------------
+--------------- 2.Conditionals if any -------------------
+---------------------------------------------------------   
+   
+BEGIN
+    -- IF (ParameterName) THEN
+    --        select_statement := '
+    --        WITH CTE_Conditional1 AS (...),
+    --        ';
+    -- ELSE
+    --        select_statement := '
+    --        WITH CTE_Conditional2 AS (...),
+    --       ';
+    -- END IF;
+
+
+---------------------------------------------------------
+----------------- 3. SQL Statements ---------------------
+---------------------------------------------------------     
+
+--- Select Statement
+-- If no conditionals:
+select_statement := 'WITH CTE_1 AS (SELECT ...), 
+                     CTE_FINAL AS (SELECT ...)
+                     
+                     SELECT * FROM CTE_FINAL';
+
+-- If conditionals:
+select_statement := select_statement || 
+                    'CTE_1 AS (SELECT ...), 
+                     CTE_FINAL AS (SELECT ...)
+                     
+                     SELECT * FROM CTE_FINAL';
+
+--- Update Statement
+update_statement := ' UPDATE 
+                     SET (columns to update)';
+
+--- Insert Statement
+insert_statement := ' INSERT  (columns to insert)
+                      VALUES (columns values)';
+
+---------------------------------------------------------
+--------- 4. Actions (Inserts and Updates) --------------
+---------------------------------------------------------  
+
+
+merge_statement := ' MERGE INTO #FinalTable# as target USING 
+                   ('||select_statement||') as source 
+                   ON source.id = target.id
+                   WHEN MATCHED THEN '||update_statement|| '
+                   WHEN NOT MATCHED THEN '||insert_statement||;
+                   
+---------------------------------------------------------
+------------------- 5. Execution ------------------------
+--------------------------------------------------------- 
+                    
+EXECUTE IMMEDIATE merge_statement ;
+
+---------------------------------------------------------
+--------------- 6. Status monitoring --------------------
+--------------------------------------------------------- 
+
+status := 'Completed successfully';
+    RETURN status;
+
+
+        
+EXCEPTION
+    WHEN OTHER THEN
+          status := 'Failed during execution. ' || 'SQL Error: ' || SQLERRM || ' Error code: ' || SQLCODE || '. SQL State: ' || SQLSTATE;
+          RETURN status;
+
+
+    
+END;
+
+
+
+
