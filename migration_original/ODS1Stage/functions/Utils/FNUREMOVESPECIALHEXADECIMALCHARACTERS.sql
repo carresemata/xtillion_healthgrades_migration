adbc06ac2783fecb42443e20271fc651d1eaafb5
carresemata <carrese@rvohealth.com>
2024-05-03 07:35:45 +0200

    function to lowercase

--- migration_original/ODS1Stage/functions/Utils/FNUREMOVESPECIALHEXADECIMALCHARACTERS.sql
+++ migration_original/ODS1Stage/functions/Utils/FNUREMOVESPECIALHEXADECIMALCHARACTERS.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE FUNCTION ODS1_STAGE.UTILS.FNUREMOVESPECIALHEXADECIMALCHARACTERS("VALUE" VARCHAR(16777216))
 RETURNS VARCHAR(16777216)
 LANGUAGE SQL
-AS '
-	SELECT REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( 
-		value, ''\\x00'','''') ,''\\x01'','''') ,''\\x02'','''') ,''\\x03'','''') ,''\\x04'','''') ,''\\x05'','''') ,''\\x06'','''') ,''\\x07'','''') ,''\\x08'','''') ,''\\x0B'','''') ,''\\x0C'','''') ,''\\x0E'','''') ,''\\x0F'','''') ,''\\x10'','''') ,''\\x11'','''') ,''\\x12'','''') ,''\\x13'','''') ,''\\x14'','''') ,''\\x15'','''') ,''\\x16'','''') ,''\\x17'','''') ,''\\x18'','''') ,''\\x19'','''') ,''\\x1A'','''') ,''\\x1B'','''') ,''\\x1C'','''') ,''\\x1D'','''') ,''\\x1E'','''') ,''\\x1F'','''')
-';
+AS 
+
+select replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( replace( 
+		value, '\\x00','') ,'\\x01','') ,'\\x02','') ,'\\x03','') ,'\\x04','') ,'\\x05','') ,'\\x06','') ,'\\x07','') ,'\\x08','') ,'\\x0b','') ,'\\x0c','') ,'\\x0e','') ,'\\x0f','') ,'\\x10','') ,'\\x11','') ,'\\x12','') ,'\\x13','') ,'\\x14','') ,'\\x15','') ,'\\x16','') ,'\\x17','') ,'\\x18','') ,'\\x19','') ,'\\x1a','') ,'\\x1b','') ,'\\x1c','') ,'\\x1d','') ,'\\x1e','') ,'\\x1f','')
+;
