b7ccb2b8cc7ba0c2697e06919094436d3e4e9543
ASanchezRVOH <asanchez@rvohealth.com>
2024-06-04 17:16:54 -0400

    updated translated procedure

--- migration_original/ODS1Stage/tables/Base/ProviderToHealthInsurance/spu_translated_ProviderToHealthInsurance.sql
+++ migration_original/ODS1Stage/tables/Base/ProviderToHealthInsurance/spu_translated_ProviderToHealthInsurance.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PROCEDURE ODS1_STAGE_TEAM.BASE.SP_LOAD_PROVIDERTOHEALTHINSURANCE("IS_FULL" BOOLEAN)
-RETURNS VARCHAR(16777216)
+RETURNS STRING
 LANGUAGE SQL
 EXECUTE AS CALLER
 AS 'declare
@@ -20,10 +20,10 @@ select_statement string; -- cte and select statement for the merge
 insert_statement string; -- insert statement for the merge
 merge_statement string; -- merge statement to final table
 status string; -- status monitoring
-procedure_name varchar(50) default(''sp_load_providertohealthinsurance'');
+procedure_name varchar(50) default('sp_load_providertohealthinsurance');
 execution_start datetime default getdate();
 
-mdm_db string default(''mdm_team'');
+mdm_db string default('mdm_team');
 
 
 
@@ -60,7 +60,7 @@ where json.provider_PROFILE is not null
 $$;
 
 --- insert Statement
-insert_statement := '' insert 
+insert_statement := ' insert 
                         (ProviderToHealthInsuranceId, 
                         ProviderId, 
                         HealthInsurancePlanToPlanTypeId, 
@@ -71,17 +71,17 @@ insert_statement := '' insert
                         source.providerid, 
                         source.healthinsuranceplantoplantypeid, 
                         source.sourcecode, 
-                        source.lastupdatedate)'';
+                        source.lastupdatedate)';
 
 ---------------------------------------------------------
 --------- 4. actions (inserts and updates) --------------
 ---------------------------------------------------------
 
-merge_statement := ''merge into base.providertohealthinsurance as target
-                    using (''||select_statement||'') as source
+merge_statement := 'merge into base.providertohealthinsurance as target
+                    using ('||select_statement||') as source
                     on source.providerid = target.providerid
                     WHEN MATCHED then delete
-                    when not matched then ''||insert_statement;
+                    when not matched then '||insert_statement;
 
 ---------------------------------------------------------
 -------------------  5. execution ------------------------
@@ -96,7 +96,7 @@ execute immediate merge_statement;
 --------------- 6. status monitoring --------------------
 ---------------------------------------------------------
 
-status := ''completed successfully'';
+status := 'completed successfully';
         insert into utils.procedure_execution_log (database_name, procedure_schema, procedure_name, status, execution_start, execution_complete) 
                 select current_database(), current_schema() , :procedure_name, :status, :execution_start, getdate(); 
 
@@ -104,10 +104,10 @@ status := ''completed successfully'';
 
         exception
         when other then
-            status := ''failed during execution. '' || ''sql error: '' || sqlerrm || '' error code: '' || sqlcode || ''. sql state: '' || sqlstate;
+            status := 'failed during execution. ' || 'sql error: ' || sqlerrm || ' error code: ' || sqlcode || '. sql state: ' || sqlstate;
 
             insert into utils.procedure_error_log (database_name, procedure_schema, procedure_name, status, err_snowflake_sqlcode, err_snowflake_sql_message, err_snowflake_sql_state) 
-                select current_database(), current_schema() , :procedure_name, :status, split_part(regexp_substr(:status, ''error code: ([0-9]+)''), '':'', 2)::integer, trim(split_part(split_part(:status, ''sql error:'', 2), ''error code:'', 1)), split_part(regexp_substr(:status, ''sql state: ([0-9]+)''), '':'', 2)::integer; 
+                select current_database(), current_schema() , :procedure_name, :status, split_part(regexp_substr(:status, 'error code: ([0-9]+)'), ':', 2)::integer, trim(split_part(split_part(:status, 'sql error:', 2), 'error code:', 1)), split_part(regexp_substr(:status, 'sql state: ([0-9]+)'), ':', 2)::integer; 
 
             return status;
 end';
