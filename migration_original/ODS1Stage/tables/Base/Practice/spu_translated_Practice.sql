3d613ed1f644bccfa04bed8a3162e5ca210dbcce
carresemata <carrese@rvohealth.com>
2024-06-05 11:53:52 -0400

    base.practice

--- migration_original/ODS1Stage/tables/Base/Practice/spu_translated_Practice.sql
+++ migration_original/ODS1Stage/tables/Base/Practice/spu_translated_Practice.sql
@@ -1,9 +1,8 @@
-CREATE or REPLACE PROCEDURE ODS1_STAGE_TEAM.BASE.SP_LOAD_PRACTICE(is_full BOOLEAN) 
-    RETURNS STRING
-    LANGUAGE SQL
-    EXECUTE as CALLER
-    as  
-declare 
+CREATE OR REPLACE PROCEDURE ODS1_STAGE_TEAM.BASE.SP_LOAD_PRACTICE("IS_FULL" BOOLEAN)
+RETURNS VARCHAR(16777216)
+LANGUAGE SQL
+EXECUTE AS CALLER
+AS 'declare 
 ---------------------------------------------------------
 --------------- 1. table dependencies -------------------
 ---------------------------------------------------------
@@ -20,9 +19,9 @@ declare
     insert_statement string; -- insert statement for the merge
     merge_statement string; -- merge statement to final table
     status string; -- status monitoring
-    procedure_name varchar(50) default('sp_load_practice');
+    procedure_name varchar(50) default(''sp_load_practice'');
     execution_start datetime default getdate();
-
+    mdm_db string default(''mdm_team'');
    
    
 begin
@@ -35,24 +34,21 @@ begin
 
 --- select Statement
 select_statement := $$ select 
-                            ifnull(json.demographics_LastUpdateDate, current_timestamp()) as LastUpdateDate,
-                            json.demographics_NPI as NPI,
-                            json.practicecode,
-                            CASE WHEN json.demographics_Logo = 'None' then null else json.demographics_Logo END as PracticeLogo,
-                            CASE WHEN json.demographics_MedicalDirector = 'None' then null else json.demographics_MedicalDirector END as PracticeMedicalDirector,
-                            CASE WHEN json.demographics_PracticeName LIKE '%&amp;%' then REPLACE(json.demographics_PracticeName, '&amp;', '&') else ifnull(json.demographics_PracticeName, '' ) END as PracticeName,
-                            ifnull(json.demographics_SourceCode, 'Profisee') as SourceCode,
-                            json.demographics_YearPracticeEstablished as YearPracticeEstablished
-                        from raw.vw_PRACTICE_PROFILE as JSON
+                            ifnull(TO_TIMESTAMP_NTZ(PRACTICE_PROFILE:DEMOGRAPHICS[0].UPDATED_DATETIME), current_timestamp()) as LastUpdateDate,
+                            TO_VARCHAR(PRACTICE_PROFILE:DEMOGRAPHICS[0].NPI) as NPI,
+                            REF_PRACTICE_CODE as practicecode,
+                            CASE WHEN TO_VARCHAR(PRACTICE_PROFILE:DEMOGRAPHICS[0].LOGO) = ''None'' then null else TO_VARCHAR(PRACTICE_PROFILE:DEMOGRAPHICS[0].LOGO) END as PracticeLogo,
+                            CASE WHEN TO_VARCHAR(PRACTICE_PROFILE:DEMOGRAPHICS[0].MEDICAL_DIRECTOR) = ''None'' then null else TO_VARCHAR(PRACTICE_PROFILE:DEMOGRAPHICS[0].MEDICAL_DIRECTOR) END as PracticeMedicalDirector,
+                            CASE WHEN TO_VARCHAR(PRACTICE_PROFILE:DEMOGRAPHICS[0].PRACTICE_NAME) LIKE ''%&amp;%'' then REPLACE(TO_VARCHAR(PRACTICE_PROFILE:DEMOGRAPHICS[0].PRACTICE_NAME), ''&amp;'', ''&'') else ifnull(TO_VARCHAR(PRACTICE_PROFILE:DEMOGRAPHICS[0].PRACTICE_NAME), '''' ) END as PracticeName,
+                            ifnull(TO_VARCHAR(PRACTICE_PROFILE:DEMOGRAPHICS[0].DATA_SOURCE_CODE), ''Profisee'') as SourceCode,
+                            TO_VARCHAR(PRACTICE_PROFILE:DEMOGRAPHICS[0].YEAR_PRACTICE_ESTABLISHED) as YearPracticeEstablished
+                        from $$ || mdm_db || $$.mst.practice_profile_processing
                         where 
-                            PRACTICE_PROFILE is not null and
-                            PracticeCode is not null and
                             PracticeName is not null and
-                            LENGTH(PracticeCode) <= 10
-                        qualify row_number() over(partition by PracticeCode order by CREATE_DATE desc) = 1$$;
+                            LENGTH(PracticeCode) <= 10 $$;
 
 --- update Statement
-update_statement := ' update
+update_statement := '' update
                         SET
                             LastUpdateDate = source.lastupdatedate,
                             NPI = source.npi,
@@ -61,10 +57,10 @@ update_statement := ' update
                             PracticeMedicalDirector = source.practicemedicaldirector,
                             PracticeName = source.practicename,
                             SourceCode = source.sourcecode,
-                            YearPracticeEstablished = source.yearpracticeestablished';
+                            YearPracticeEstablished = source.yearpracticeestablished'';
 
 --- insert Statement
-insert_statement := ' insert 
+insert_statement := '' insert 
                             (
                             PracticeID,
                             LastUpdateDate,
@@ -87,18 +83,18 @@ insert_statement := ' insert
                             source.practicename,
                             source.sourcecode,
                             source.yearpracticeestablished
-                            )';
+                            )'';
 
 ---------------------------------------------------------
 --------- 4. actions (inserts and updates) --------------
 ---------------------------------------------------------  
 
 
-merge_statement := ' merge into base.practice as target using 
-                   ('||select_statement||') as source 
+merge_statement := '' merge into base.practice as target using 
+                   (''||select_statement||'') as source 
                    on source.practicecode = target.practicecode
-                   WHEN MATCHED then '||update_statement|| '
-                   when not matched then '||insert_statement;
+                   WHEN MATCHED then ''||update_statement|| ''
+                   when not matched then ''||insert_statement;
                    
 ---------------------------------------------------------
 -------------------  5. execution ------------------------
@@ -113,7 +109,7 @@ execute immediate merge_statement ;
 --------------- 6. status monitoring --------------------
 --------------------------------------------------------- 
 
-status := 'completed successfully';
+status := ''completed successfully'';
         insert into utils.procedure_execution_log (database_name, procedure_schema, procedure_name, status, execution_start, execution_complete) 
                 select current_database(), current_schema() , :procedure_name, :status, :execution_start, getdate(); 
 
@@ -121,10 +117,10 @@ status := 'completed successfully';
 
         exception
         when other then
-            status := 'failed during execution. ' || 'sql error: ' || sqlerrm || ' error code: ' || sqlcode || '. sql state: ' || sqlstate;
+            status := ''failed during execution. '' || ''sql error: '' || sqlerrm || '' error code: '' || sqlcode || ''. sql state: '' || sqlstate;
 
             insert into utils.procedure_error_log (database_name, procedure_schema, procedure_name, status, err_snowflake_sqlcode, err_snowflake_sql_message, err_snowflake_sql_state) 
-                select current_database(), current_schema() , :procedure_name, :status, split_part(regexp_substr(:status, 'error code: ([0-9]+)'), ':', 2)::integer, trim(split_part(split_part(:status, 'sql error:', 2), 'error code:', 1)), split_part(regexp_substr(:status, 'sql state: ([0-9]+)'), ':', 2)::integer; 
+                select current_database(), current_schema() , :procedure_name, :status, split_part(regexp_substr(:status, ''error code: ([0-9]+)''), '':'', 2)::integer, trim(split_part(split_part(:status, ''sql error:'', 2), ''error code:'', 1)), split_part(regexp_substr(:status, ''sql state: ([0-9]+)''), '':'', 2)::integer; 
 
             return status;
-end;
+end';
