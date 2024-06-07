149da5e34aa070df78b9adb3c4110ddb761b41ae
carresemata <carrese@rvohealth.com>
2024-06-07 09:43:17 -0400

    base.facilityhours

--- migration_original/ODS1Stage/tables/Base/FacilityHours/spu_translated_FacilityHours.sql
+++ migration_original/ODS1Stage/tables/Base/FacilityHours/spu_translated_FacilityHours.sql
@@ -23,12 +23,10 @@ declare
     status string; -- status monitoring
     procedure_name varchar(50) default('sp_load_facilityhours');
     execution_start datetime default getdate();
-
-   
+    mdm_db string default('mdm_team');
    
 begin
     
-
 
 ---------------------------------------------------------
 ----------------- 3. SQL Statements ---------------------
@@ -36,7 +34,22 @@ begin
 
 --- select Statement
 -- if no conditionals:
-select_statement := $$ select distinct
+select_statement := $$ with Cte_hours as (
+                                select
+                                    p.ref_facility_code as facilitycode,
+                                    to_varchar(json.value:DAYS_OF_WEEK_CODE) as Hours_DaysOfWeek,
+                                    to_varchar(json.value:OPENING_TIME) as Hours_OpeningTime,
+                                    to_varchar(json.value:CLOSING_TIME) as Hours_ClosingTime,
+                                    to_boolean(json.value:IS_CLOSED) as Hours_IsClosed,
+                                    to_boolean(json.value:IS_OPEN_24_HOURS) as Hours_IsOpen24Hours,
+                                    to_varchar(json.value:DATA_SOURCE_CODE) as Hours_SourceCode,
+                                    to_timestamp_ntz(json.value:UPDATED_DATETIME) as Hours_LastUpdateDate
+                                from $$ || mdm_db || $$.mst.facility_profile_processing as p
+                                , lateral flatten(input => p.FACILITY_PROFILE:HOURS) as json
+                                where Hours_SourceCode = 'HG INST'
+                            )
+
+                    select distinct
                         facility.facilityid,
                         ifnull(json.hours_SourceCode, 'Profisee') as SourceCode,
                         days.daysofweekid,
@@ -44,18 +57,11 @@ select_statement := $$ select distinct
                         json.hours_ClosingTime as FacilityHoursClosingTime,
                         json.hours_IsClosed as FacilityIsClosed,
                         json.hours_IsOpen24Hours as FacilityIsOpen24Hours,
-                        ifnull(json.hours_LastUpdateDate, CAST(current_timestamp() as TIMESTAMP_NTZ(3))) as LastUpdateDate
+                        ifnull(json.hours_LastUpdateDate, current_timestamp()) as LastUpdateDate
                     from
-                        raw.vw_FACILITY_PROFILE as JSON
-                        left join base.facility as Facility on json.facilitycode = facility.facilitycode
-                        left join base.daysofweek as Days on days.daysofweekcode = json.hours_DaysOfWeek
-                    where
-                        json.facility_PROFILE is not null and
-                        FacilityId is not null and
-                        DaysOFWeekID is not null 
-                        qualify row_number() over(partition by facility.facilityid, Hours_DaysOfWeek
-                                                    order by
-                                                    CREATE_DATE desc) = 1 $$;
+                        cte_hours as json
+                        join base.facility as Facility on json.facilitycode = facility.facilitycode
+                        join base.daysofweek as Days on days.daysofweekcode = json.hours_DaysOfWeek $$;
 
 
 --- insert Statement
@@ -86,8 +92,7 @@ insert_statement := ' insert
 
 merge_statement := $$ merge into base.facilityhours as target using 
                    ($$ ||select_statement|| $$) as source 
-                   on source.facilityid = target.facilityid
-                   WHEN MATCHED and source.sourcecode != 'HG INST' then delete
+                   on source.facilityid = target.facilityid and source.daysofweekid = target.daysofweekid
                    when not matched then $$ ||insert_statement;
                    
 ---------------------------------------------------------
