4e5cb1842ad1f3ea7e14ed0bcb3d0935567db9e1
carresemata <carrese@rvohealth.com>
2024-06-19 12:14:33 -0400

    add report

--- migration_original/ODS1Stage/tables/Base/Provider/spu_translated_Provider.sql
+++ migration_original/ODS1Stage/tables/Base/Provider/spu_translated_Provider.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE PROCEDURE ODS1_STAGE_TEAM.BASE.SP_LOAD_PROVIDER("IS_FULL" BOOLEAN)
+CREATE OR REPLACE PROCEDURE ODS1_STAGE_TEAM.BASE.SP_LOAD_PROVIDER(is_full BOOLEAN)
 RETURNS VARCHAR(16777216)
 LANGUAGE SQL
 EXECUTE AS CALLER
-AS 'declare 
+AS declare 
 ---------------------------------------------------------
 --------------- 1. table dependencies -------------------
 ---------------------------------------------------------
@@ -23,10 +23,9 @@ AS 'declare
 
     update_statement_2 string; -- update statement for the merge
     status string; -- status monitoring
-    procedure_name varchar(50) default(''sp_load_provider'');
+    procedure_name varchar(50) default('sp_load_provider');
     execution_start datetime default getdate();
-    mdm_db string default(''mdm_team'');
-   
+    mdm_db string default('mdm_team');
    
 begin
     
@@ -39,7 +38,6 @@ begin
 --- select Statement
 select_statement_1 := $$ select
                             -- ReltioEntityId
-                            uuid_string() as ProviderId,
                             -- EDWBaseRecordID
                             json.ref_provider_code as providercode,
                             to_varchar(json.PROVIDER_PROFILE:DEMOGRAPHICS[0]:FIRST_NAME) as FirstName,
@@ -55,7 +53,7 @@ select_statement_1 := $$ select
                             to_varchar(json.PROVIDER_PROFILE:DEMOGRAPHICS[0]:ACCEPTS_NEW_PATIENTS) as AcceptsNewPatients,
                             -- HasElectronicMedicalRecords,
                             -- HasElectronicPrescription,
-                            ifnull(json.PROVIDER_PROFILE:DEMOGRAPHICS[0]:DATA_SOURCE_CODE, ''Profisee'') as SourceCode,
+                            ifnull(json.PROVIDER_PROFILE:DEMOGRAPHICS[0]:DATA_SOURCE_CODE, 'Profisee') as SourceCode,
                             s.sourceid,
                             ifnull(json.PROVIDER_PROFILE:DEMOGRAPHICS[0]:LAST_UPDATE_DATE, sysdate()) as LastUpdateDate,
                             -- PatientVolume
@@ -67,13 +65,13 @@ select_statement_1 := $$ select
                             to_varchar(json.PROVIDER_PROFILE:DEMOGRAPHICS[0]:IS_PATIENT_FAVORITE) as IsPatientFavorite
                         from
                             $$ || mdm_db || $$.mst.provider_profile_processing as JSON
-                            left join ((select distinct(sourcecode), sourceid from base.source where lastupdatedate != ''NaT'')) as S on s.sourcecode = to_varchar(json.PROVIDER_PROFILE:DEMOGRAPHICS[0]:DATA_SOURCE_CODE) $$;
+                            left join ((select distinct(sourcecode), sourceid from base.source where lastupdatedate != 'NaT')) as S on s.sourcecode = to_varchar(json.PROVIDER_PROFILE:DEMOGRAPHICS[0]:DATA_SOURCE_CODE) $$;
 
 
 
 --- update Statement
-update_statement_1 := '' update 
-                     SET  target.providercode = source.providercode,
+update_statement_1 := ' update 
+                            SET
                             target.firstname = source.firstname,
                             target.middlename = source.middlename,
                             target.lastname = source.lastname,
@@ -86,25 +84,26 @@ update_statement_1 := '' update
                             target.sourceid = source.sourceid,
                             target.lastupdatedate = source.lastupdatedate,
                             target.surviveresidentialaddresses = source.surviveresidentialaddresses,
-                            target.ispatientfavorite = source.ispatientfavorite'';
+                            target.ispatientfavorite = source.ispatientfavorite';
+                            
 -- update Clause
-update_clause_1 := $$ ifnull(target.providercode, '''') != ifnull(source.providercode, '''')
-        or ifnull(target.firstname, '''') != ifnull(source.firstname, '''')
-        or ifnull(target.middlename, '''') != ifnull(source.middlename, '''')
-        or ifnull(target.lastname, '''') != ifnull(source.lastname, '''')
-        or ifnull(target.suffix, '''') != ifnull(source.suffix, '''')
-        or ifnull(target.gender, '''') != ifnull(source.gender, '''')
-        or ifnull(target.npi, '''') != ifnull(source.npi, '''')
-        or ifnull(target.dateofbirth, ''1900-01-01'') != ifnull(source.dateofbirth,''1900-01-01'')
+update_clause_1 := $$ ifnull(target.providercode, '') != ifnull(source.providercode, '')
+        or ifnull(target.firstname, '') != ifnull(source.firstname, '')
+        or ifnull(target.middlename, '') != ifnull(source.middlename, '')
+        or ifnull(target.lastname, '') != ifnull(source.lastname, '')
+        or ifnull(target.suffix, '') != ifnull(source.suffix, '')
+        or ifnull(target.gender, '') != ifnull(source.gender, '')
+        or ifnull(target.npi, '') != ifnull(source.npi, '')
+        or ifnull(target.dateofbirth, '1900-01-01') != ifnull(source.dateofbirth,'1900-01-01')
         or ifnull(target.acceptsnewpatients, 0) != ifnull(source.acceptsnewpatients, 0)
-        or ifnull(target.sourcecode, '''') != ifnull(source.sourcecode, '''')
-        or ifnull(target.sourceid, ''00000000-0000-0000-0000-000000000000'') != ifnull(source.sourceid,''00000000-0000-0000-0000-000000000000'')
-        or ifnull(target.lastupdatedate, ''1900-01-01'') != ifnull(source.lastupdatedate, ''1900-01-01'')
+        or ifnull(target.sourcecode, '') != ifnull(source.sourcecode, '')
+        or ifnull(target.sourceid, '00000000-0000-0000-0000-000000000000') != ifnull(source.sourceid,'00000000-0000-0000-0000-000000000000')
+        or ifnull(target.lastupdatedate, '1900-01-01') != ifnull(source.lastupdatedate, '1900-01-01')
         or ifnull(target.surviveresidentialaddresses, 0) != ifnull(source.surviveresidentialaddresses, 0)
         or ifnull(target.ispatientfavorite, 0) != ifnull(source.ispatientfavorite, 0) $$;                        
         
 --- insert Statement
-insert_statement_1 := '' insert  
+insert_statement_1 := ' insert  
                             (ProviderID,
                             ProviderCode,
                             FirstName,
@@ -121,7 +120,7 @@ insert_statement_1 := '' insert
                             SurviveResidentialAddresses,
                             IsPatientFavorite)
                       values 
-                          ( source.providerid,
+                          ( uuid_string(),
                             source.providercode,
                             source.firstname,
                             source.middlename,
@@ -135,11 +134,11 @@ insert_statement_1 := '' insert
                             source.sourceid,
                             source.lastupdatedate,
                             source.surviveresidentialaddresses,
-                            source.ispatientfavorite)'';
+                            source.ispatientfavorite)';
 
 -- update Statement
 
-update_statement_2 := $$update base.provider as target
+update_statement_2 := $$ update base.provider as target
        SET target.carephilosophy = source.provideraboutmetext
          from (select 
                     json.ref_provider_code as providercode,
@@ -147,7 +146,7 @@ update_statement_2 := $$update base.provider as target
                     to_varchar(aboutme.VALUE:ABOUT_ME_TEXT) as ProviderAboutMeText
                 from $$ || mdm_db || $$.mst.provider_profile_processing as JSON
                     , lateral flatten (input => json.PROVIDER_PROFILE:ABOUT_ME) ABOUTME
-                    where AboutMeCode = ''CarePhilosophy'') as source
+                    where AboutMeCode = 'CarePhilosophy') as source
          where target.providercode = source.providercode
     $$;
     
@@ -155,11 +154,11 @@ update_statement_2 := $$update base.provider as target
 --------- 4. actions (inserts and updates) --------------
 ---------------------------------------------------------  
 
-merge_statement_1 := '' merge into base.provider as target using 
-                   (''||select_statement_1||'') as source 
+merge_statement_1 := ' merge into base.provider as target using 
+                   ('||select_statement_1||') as source 
                    on source.providercode = target.providercode
-                   WHEN MATCHED and'' || update_clause_1 || ''then ''||update_statement_1|| ''
-                   when not matched then ''||insert_statement_1;
+                   WHEN MATCHED and' || update_clause_1 || 'then '||update_statement_1|| '
+                   when not matched then '||insert_statement_1;
                    
 ---------------------------------------------------------
 -------------------  5. execution ------------------------
@@ -175,7 +174,7 @@ execute immediate update_statement_2;
 --------------- 6. status monitoring --------------------
 --------------------------------------------------------- 
 
-status := ''completed successfully'';
+status := 'completed successfully';
         insert into utils.procedure_execution_log (database_name, procedure_schema, procedure_name, status, execution_start, execution_complete) 
                 select current_database(), current_schema() , :procedure_name, :status, :execution_start, getdate(); 
 
@@ -183,10 +182,10 @@ status := ''completed successfully'';
 
         exception
         when other then
-            status := ''failed during execution. '' || ''sql error: '' || sqlerrm || '' error code: '' || sqlcode || ''. sql state: '' || sqlstate;
+            status := 'failed during execution. ' || 'sql error: ' || sqlerrm || ' error code: ' || sqlcode || '. sql state: ' || sqlstate;
 
             insert into utils.procedure_error_log (database_name, procedure_schema, procedure_name, status, err_snowflake_sqlcode, err_snowflake_sql_message, err_snowflake_sql_state) 
-                select current_database(), current_schema() , :procedure_name, :status, split_part(regexp_substr(:status, ''error code: ([0-9]+)''), '':'', 2)::integer, trim(split_part(split_part(:status, ''sql error:'', 2), ''error code:'', 1)), split_part(regexp_substr(:status, ''sql state: ([0-9]+)''), '':'', 2)::integer; 
+                select current_database(), current_schema() , :procedure_name, :status, split_part(regexp_substr(:status, 'error code: ([0-9]+)'), ':', 2)::integer, trim(split_part(split_part(:status, 'sql error:', 2), 'error code:', 1)), split_part(regexp_substr(:status, 'sql state: ([0-9]+)'), ':', 2)::integer; 
 
             return status;
-end';
+end;
