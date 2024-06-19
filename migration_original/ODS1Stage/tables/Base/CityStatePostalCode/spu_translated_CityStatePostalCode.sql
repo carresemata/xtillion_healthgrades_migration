d6fb5577dcf04fe0e3d0f8ddbf7c22f44efdc320
carresemata <161375798+carresemata@users.noreply.github.com>
2024-06-19 14:03:43 -0400

    Update spu_translated_CityStatePostalCode.sql

--- migration_original/ODS1Stage/tables/Base/CityStatePostalCode/spu_translated_CityStatePostalCode.sql
+++ migration_original/ODS1Stage/tables/Base/CityStatePostalCode/spu_translated_CityStatePostalCode.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE PROCEDURE ODS1_STAGE_TEAM.BASE.SP_LOAD_CITYSTATEPOSTALCODE("IS
 RETURNS VARCHAR(16777216)
 LANGUAGE SQL
 EXECUTE AS CALLER
-AS declare 
+AS 'declare 
 ---------------------------------------------------------
 --------------- 1. table dependencies -------------------
 ---------------------------------------------------------
@@ -24,9 +24,9 @@ AS declare
     insert_statement_2 string; -- insert statement for the merge
     merge_statement_2 string; -- merge statement to final table
     status string; -- status monitoring
-    procedure_name varchar(50) default('sp_load_citystatepostalcode');
+    procedure_name varchar(50) default(''sp_load_citystatepostalcode'');
     execution_start datetime default getdate();
-    mdm_db string default('mdm_team');
+    mdm_db string default(''mdm_team'');
    
    
 begin
@@ -39,37 +39,35 @@ begin
 
 --- select Statement
 select_statement_1 := $$ select distinct
-                                CASE WHEN trim(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].CITY)) LIKE '%,' then LEFT(trim(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].CITY)), LENGTH(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].CITY))-1) else TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].CITY) END as City,
+                                CASE WHEN trim(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].CITY)) LIKE ''%,'' then LEFT(trim(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].CITY)), LENGTH(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].CITY))-1) else TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].CITY) END as City,
                                 TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].STATE) as State,
                                 TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].ZIP) as PostalCode
                             from
                                 $$ || mdm_db || $$.mst.office_profile_processing as process
                             where
-                                    nullif(City,'') is not null 
-                                    and nullif(State,'') is not null 
-                                    and nullif(Postalcode,'') is not null
-                                    and LENGTH(trim(upper(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].ADDRESS_LINE_1))) || ifnull(trim(upper(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].ADDRESS_LINE_2))),'') || ifnull(trim(upper(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].SUITE))),'')) > 0
+                                    nullif(City,'''') is not null 
+                                    and nullif(State,'''') is not null 
+                                    and nullif(Postalcode,'''') is not null
+                                    and LENGTH(trim(upper(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].ADDRESS_LINE_1))) || ifnull(trim(upper(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].ADDRESS_LINE_2))),'''') || ifnull(trim(upper(TO_VARCHAR(Process.OFFICE_PROFILE:ADDRESS[0].SUITE))),'''')) > 0
                                          $$;
 
 
 
 --- insert Statement
-insert_statement_1 := $$ insert (
+insert_statement_1 := '' insert (
                                 CityStatePostalCodeId,
                                 City,
                                 State,
                                 PostalCode,
-                                LastUpdateDate,
-                                nationid
+                                LastUpdateDate
                         )
                         values (
                                 uuid_string(),
                                 source.city,
                                 source.state,
                                 source.postalcode,
-                                current_timestamp(),
-                                '00415355-0000-0000-0000-000000000000'
-                        )$$;
+                                current_timestamp()
+                        )'';
 
 select_statement_2 := $$ select distinct
                                     TO_VARCHAR(Process.FACILITY_PROFILE:ADDRESS[0].CITY) as City,
@@ -81,9 +79,9 @@ select_statement_2 := $$ select distinct
                                     $$ || mdm_db || $$.mst.facility_profile_processing as process 
                                     join base.facility as Facility on process.ref_facility_code = facility.facilitycode
                                 where
-                                    nullif(City,'') is not null 
-                                    and nullif(State,'') is not null 
-                                    and nullif(PostalCode,'') is not null $$;
+                                    nullif(City,'''') is not null 
+                                    and nullif(State,'''') is not null 
+                                    and nullif(PostalCode,'''') is not null $$;
 
 insert_statement_2 := $$insert (
                             CityStatePostalCodeId, 
@@ -102,7 +100,7 @@ insert_statement_2 := $$insert (
                             source.postalcode, 
                             source.latitude, 
                             source.longitude, 
-                            '00415355-0000-0000-0000-000000000000', 
+                            ''00415355-0000-0000-0000-000000000000'', 
                             current_timestamp()
                         );$$;
 
@@ -111,15 +109,15 @@ insert_statement_2 := $$insert (
 ---------------------------------------------------------  
 
 
-merge_statement_1 := ' merge into base.citystatepostalcode as target using 
-                   ('||select_statement_1||') as source 
+merge_statement_1 := '' merge into base.citystatepostalcode as target using 
+                   (''||select_statement_1||'') as source 
                    on source.city = target.city and source.state = target.state and source.postalcode = target.postalcode
-                   when not matched then '||insert_statement_1;
+                   when not matched then ''||insert_statement_1;
 
-merge_statement_2 := ' merge into base.citystatepostalcode as target using 
-                   ('||select_statement_2||') as source 
+merge_statement_2 := '' merge into base.citystatepostalcode as target using 
+                   (''||select_statement_2||'') as source 
                    on source.city = target.city and source.state = target.state and source.postalcode = target.postalcode
-                   when not matched then '||insert_statement_2;
+                   when not matched then ''||insert_statement_2;
                    
 ---------------------------------------------------------
 -------------------  5. execution ------------------------
@@ -135,7 +133,7 @@ execute immediate merge_statement_2 ;
 --------------- 6. status monitoring --------------------
 --------------------------------------------------------- 
 
-status := 'completed successfully';
+status := ''completed successfully'';
         insert into utils.procedure_execution_log (database_name, procedure_schema, procedure_name, status, execution_start, execution_complete) 
                 select current_database(), current_schema() , :procedure_name, :status, :execution_start, getdate(); 
 
@@ -143,10 +141,10 @@ status := 'completed successfully';
 
         exception
         when other then
-            status := 'failed during execution. ' || 'sql error: ' || sqlerrm || ' error code: ' || sqlcode || '. sql state: ' || sqlstate;
+            status := ''failed during execution. '' || ''sql error: '' || sqlerrm || '' error code: '' || sqlcode || ''. sql state: '' || sqlstate;
 
             insert into utils.procedure_error_log (database_name, procedure_schema, procedure_name, status, err_snowflake_sqlcode, err_snowflake_sql_message, err_snowflake_sql_state) 
-                select current_database(), current_schema() , :procedure_name, :status, split_part(regexp_substr(:status, 'error code: ([0-9]+)'), ':', 2)::integer, trim(split_part(split_part(:status, 'sql error:', 2), 'error code:', 1)), split_part(regexp_substr(:status, 'sql state: ([0-9]+)'), ':', 2)::integer; 
+                select current_database(), current_schema() , :procedure_name, :status, split_part(regexp_substr(:status, ''error code: ([0-9]+)''), '':'', 2)::integer, trim(split_part(split_part(:status, ''sql error:'', 2), ''error code:'', 1)), split_part(regexp_substr(:status, ''sql state: ([0-9]+)''), '':'', 2)::integer; 
 
             return status;
-end;
+end';
