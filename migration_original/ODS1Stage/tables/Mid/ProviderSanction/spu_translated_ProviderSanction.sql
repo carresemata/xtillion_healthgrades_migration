5b877d9333a59ab1c39d0451ff8cd41a1a555fb3
carresemata <carrese@rvohealth.com>
2024-06-24 17:00:50 -0400

    mid.providersanction

--- migration_original/ODS1Stage/tables/Mid/ProviderSanction/spu_translated_ProviderSanction.sql
+++ migration_original/ODS1Stage/tables/Mid/ProviderSanction/spu_translated_ProviderSanction.sql
@@ -40,12 +40,12 @@ begin
 --- select Statement
 
 select_statement := $$ with CTE_ProviderBatch as (
-                select
-                    p.providerid
-                from
-                    $$ || mdm_db || $$.mst.Provider_Profile_Processing as ppp
-                    join base.provider as P on p.providercode = ppp.ref_provider_code),
-                    CTE_ProviderSanction as (
+                            select
+                                p.providerid
+                            from
+                                $$ || mdm_db || $$.mst.Provider_Profile_Processing as ppp
+                                join base.provider as P on p.providercode = ppp.ref_provider_code)
+                                
                             select
                                 ps.providersanctionid,
                                 ps.providerid,
@@ -61,8 +61,7 @@ select_statement := $$ with CTE_ProviderBatch as (
                                 sa.sanctionactiondescription,
                                 sat.sanctionactiontypecode,
                                 sat.sanctionactiontypedescription,
-                                s.statename as StateFull,
-                                0 as ActionCode
+                                s.statename as StateFull
                             from
                                 CTE_ProviderBatch as pb
                                 join base.providersanction as ps on ps.providerid = pb.providerid
@@ -71,76 +70,11 @@ select_statement := $$ with CTE_ProviderBatch as (
                                 join base.sanctionaction as sa on ps.sanctionactionid = sa.sanctionactionid
                                 join base.statereportingagency as sra on ps.statereportingagencyid = sra.statereportingagencyid
                                 left join base.sanctionactiontype as sat on sa.sanctionactiontypeid = sat.sanctionactiontypeid
-                                left join base.state as s on sra.state = s.state
-                        ),
-                        -- insert Action
-                        CTE_Action_1 as (
-                            select
-                                cte.providersanctionid,
-                                1 as ActionCode
-                            from
-                                CTE_ProviderSanction as cte
-                                left join mid.providersanction as mid on cte.providersanctionid = mid.providersanctionid
-                            where
-                                mid.providersanctionid is null
-                        ),
-                        -- update Action
-                        CTE_Action_2 as (
-                            select
-                                cte.providersanctionid,
-                                2 as ActionCode
-                            from
-                                CTE_ProviderSanction as cte
-                                join mid.providersanction as mid on cte.providersanctionid = mid.providersanctionid
-                           where 
-                                MD5(ifnull(cte.providerid::varchar,'')) <> MD5(ifnull(mid.providerid::varchar,'')) or
-                                MD5(ifnull(cte.sanctiondescription::varchar,'')) <> MD5(ifnull(mid.sanctiondescription::varchar,'')) or
-                                MD5(ifnull(cte.sanctiondate::varchar,'')) <> MD5(ifnull(mid.sanctiondate::varchar,'')) or
-                                MD5(ifnull(cte.reinstatementdate::varchar,'')) <> MD5(ifnull(mid.reinstatementdate::varchar,'')) or
-                                MD5(ifnull(cte.sanctiontypecode::varchar,'')) <> MD5(ifnull(mid.sanctiontypecode::varchar,'')) or
-                                MD5(ifnull(cte.sanctiontypedescription::varchar,'')) <> MD5(ifnull(mid.sanctiontypedescription::varchar,'')) or
-                                MD5(ifnull(cte.sanctioncategorycode::varchar,'')) <> MD5(ifnull(mid.sanctioncategorycode::varchar,'')) or
-                                MD5(ifnull(cte.sanctioncategorydescription::varchar,'')) <> MD5(ifnull(mid.sanctioncategorydescription::varchar,'')) or
-                                MD5(ifnull(cte.sanctionactioncode::varchar,'')) <> MD5(ifnull(mid.sanctionactioncode::varchar,'')) or
-                                MD5(ifnull(cte.sanctionactiondescription::varchar,'')) <> MD5(ifnull(mid.sanctionactiondescription::varchar,'')) or
-                                MD5(ifnull(cte.sanctionactiontypecode::varchar,'')) <> MD5(ifnull(mid.sanctionactiontypecode::varchar,'')) or
-                                MD5(ifnull(cte.sanctionactiontypedescription::varchar,'')) <> MD5(ifnull(mid.sanctionactiontypedescription::varchar,'')) or
-                                MD5(ifnull(cte.statefull::varchar,'')) <> MD5(ifnull(mid.statefull::varchar,'')) 
-                        )
-                        select
-                            distinct A0.ProviderSanctionID,
-                            A0.ProviderID,
-                            A0.SanctionDescription,
-                            A0.SanctionDate,
-                            A0.ReinstatementDate,
-                            A0.SanctionTypeCode,
-                            A0.SanctionTypeDescription,
-                            A0.SanctionCategoryCode,
-                            A0.SanctionCategoryDescription,
-                            A0.SanctionActionCode,
-                            A0.SanctionActionDescription,
-                            A0.SanctionActionTypeCode,
-                            A0.SanctionActionTypeDescription,
-                            A0.StateFull,
-                            ifnull(
-                                A1.ActionCode,
-                                ifnull(A2.ActionCode, A0.ActionCode)
-                            ) as ActionCode
-                        from
-                            CTE_ProviderSanction as A0
-                            left join CTE_Action_1 as A1 on A0.ProviderSanctionID = A1.ProviderSanctionID
-                            left join CTE_Action_2 as A2 on A0.ProviderSanctionID = A2.ProviderSanctionID
-                        where
-                            ifnull(
-                                A1.ActionCode,
-                                ifnull(A2.ActionCode, A0.ActionCode)
-                            ) <> 0 $$;
+                                left join base.state as s on sra.state = s.state $$;
 
 --- update Statement
 update_statement := ' update 
                      SET 
-                        ProviderSanctionID = source.providersanctionid,
-                        ProviderID = source.providerid,
                         SanctionDescription = source.sanctiondescription,
                         SanctionDate = source.sanctiondate,
                         ReinstatementDate = source.reinstatementdate,
@@ -193,9 +127,9 @@ insert_statement := ' insert  (
 
 merge_statement := ' merge into mid.providersanction as target using 
                    ('||select_statement||') as source 
-                   on source.providersanctionid = target.providersanctionid
-                   WHEN MATCHED and source.actioncode = 2 then '||update_statement|| '
-                   when not matched and source.actioncode = 1 then '||insert_statement;
+                   on source.providersanctionid = target.providersanctionid and source.providerid = target.providerid
+                   when matched then '||update_statement|| '
+                   when not matched then '||insert_statement;
                    
 ---------------------------------------------------------
 -------------------  5. execution ------------------------
