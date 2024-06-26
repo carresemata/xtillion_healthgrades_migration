96e36b1cf93cf8e4d0de92c8bba6be993cf7314a
carresemata <carrese@rvohealth.com>
2024-06-26 14:48:30 -0400

    prov edu

--- migration_original/ODS1Stage/tables/Mid/ProviderEducation/spu_translated_ProviderEducation.sql
+++ migration_original/ODS1Stage/tables/Mid/ProviderEducation/spu_translated_ProviderEducation.sql
@@ -1,4 +1,4 @@
-CREATE or REPLACE PROCEDURE ODS1_STAGE_TEAM.MID.SP_LOAD_PROVIDERAFFILIATION(is_full BOOLEAN)
+CREATE or REPLACE PROCEDURE ODS1_STAGE_TEAM.MID.SP_LOAD_PROVIDEREDUCATION(is_full BOOLEAN)
     RETURNS STRING
     LANGUAGE SQL
     EXECUTE as CALLER
@@ -8,12 +8,16 @@ declare
 --------------- 1. table dependencies -------------------
 ---------------------------------------------------------
     
--- mid.provideraffiliation depends on: 
+-- mid.providereducation depends on:
 --- mdm_team.mst.provider_profile_processing
+--- base.providertoeducationinstitution
+--- base.educationinstitution
+--- base.educationinstitutiontype
+--- base.address
+--- base.citystatepostalcode
+--- base.nation
+--- base.degree
 --- base.provider
---- base.providertoaffiliation
---- base.affiliation
---- base.providerrole
 
 ---------------------------------------------------------
 --------------- 2. declaring variables ------------------
@@ -24,7 +28,7 @@ declare
     insert_statement string; -- insert statement for the merge
     merge_statement string; -- merge statement to final table
     status string; -- status monitoring
-    procedure_name varchar(50) default('sp_load_provideraffiliation');
+    procedure_name varchar(50) default('sp_load_providereducation');
     execution_start datetime default getdate();
     mdm_db string default('mdm_team');
 
@@ -32,74 +36,85 @@ declare
 ----------------- 3. SQL Statements ---------------------
 ---------------------------------------------------------     
 
-begin
 --- select Statement
-
-select_statement := $$ with CTE_ProviderBatch as (
-                        select
-                            p.providerid
-                        from
-                            $$ || mdm_db || $$.mst.Provider_Profile_Processing as ppp
-                            join base.provider as P on p.providercode = ppp.ref_provider_code)
-                        select
-                            pta.providertoaffiliationid,
-                            pta.providerid,
-                            pta.affiliationbegindate,
-                            pta.affiliationenddate,
-                            pta.affiliationname,
-                            aff.affiliationtypecode,
-                            aff.affiliationtypedescription,
-                            pr.rolecode,
-                            pr.roledescription
-                        from
-                            CTE_ProviderBatch as pb
-                            join base.providertoaffiliation as pta on pta.providerid = pb.providerid
-                            join base.affiliation as aff on pta.affiliationid = aff.affiliationid
-                            join base.providerrole as pr on pta.providerroleid = pr.providerroleid $$;
+begin
+select_statement := $$
+                    with CTE_ProviderBatch as (
+                    select
+                        p.providerid
+                    from
+                        $$ || mdm_db || $$.mst.Provider_Profile_Processing as ppp
+                        join base.provider as P on p.providercode = ppp.ref_provider_code)
+                    select distinct 
+                        ptei.providertoeducationinstitutionid, 
+                        ptei.providerid, 
+                        ei.educationinstitutionname, 
+                        eit.educationinstitutiontypecode, 
+                        eit.educationinstitutiontypedescription,
+                        ptei.graduationyear, 
+                        ptei.positionheld, 
+                        d.degreeabbreviation, 
+                        csp.city, 
+                        csp.state, 
+                        n.nationname
+                    from CTE_ProviderBatch as pb 
+                        join base.providertoeducationinstitution ptei on ptei.providerid = pb.providerid
+                        join base.educationinstitution ei on ei.educationinstitutionid = ptei.educationinstitutionid
+                        join base.educationinstitutiontype eit on eit.educationinstitutiontypeid = ptei.educationinstitutiontypeid
+                        left join base.address a on a.addressid = ei.addressid
+                        left join base.citystatepostalcode csp on a.citystatepostalcodeid = csp.citystatepostalcodeid
+                        left join base.nation n on csp.nationid = n.nationid
+                        left join base.degree d on d.degreeid = ptei.degreeid $$;
 
 --- update Statement
 update_statement := ' update 
                      SET 
-                        ProviderToAffiliationID = source.providertoaffiliationid,
-                        ProviderID = source.providerid,
-                        AffiliationBeginDate = source.affiliationbegindate,
-                        AffiliationEndDate = source.affiliationenddate,
-                        AffiliationName = source.affiliationname,
-                        AffiliationTypeCode = source.affiliationtypecode,
-                        AffiliationTypeDescription = source.affiliationtypedescription,
-                        RoleCode = source.rolecode,
-                        RoleDescription = source.roledescription';
+                        ProviderToEducationInstitutionID = source.providertoeducationinstitutionid, 
+                        ProviderID = source.providerid, 
+                        EducationInstitutionName = source.educationinstitutionname, 
+                        EducationInstitutionTypeCode = source.educationinstitutiontypecode, 
+                        EducationInstitutionTypeDescription = source.educationinstitutiontypedescription,
+                        GraduationYear = source.graduationyear, 
+                        PositionHeld = source.positionheld, 
+                        DegreeAbbreviation = source.degreeabbreviation, 
+                        City = source.city, 
+                        State = source.state, 
+                        NationName = source.nationname';
 
 --- insert Statement
-insert_statement := ' insert  (
-                            ProviderToAffiliationID,
-                            ProviderID,
-                            AffiliationBeginDate,
-                            AffiliationEndDate,
-                            AffiliationName,
-                            AffiliationTypeCode,
-                            AffiliationTypeDescription,
-                            RoleCode,
-                            RoleDescription)
-                      values (
-                            source.providertoaffiliationid,
-                            source.providerid,
-                            source.affiliationbegindate,
-                            source.affiliationenddate,
-                            source.affiliationname,
-                            source.affiliationtypecode,
-                            source.affiliationtypedescription,
-                            source.rolecode,
-                            source.roledescription)';
+insert_statement := ' insert  
+                        (ProviderToEducationInstitutionID,
+                        ProviderID,
+                        EducationInstitutionName,
+                        EducationInstitutionTypeCode,
+                        EducationInstitutionTypeDescription,
+                        GraduationYear,
+                        PositionHeld,
+                        DegreeAbbreviation,
+                        City,
+                        State,
+                        NationName)
+                      values 
+                        (source.providertoeducationinstitutionid,
+                        source.providerid,
+                        source.educationinstitutionname,
+                        source.educationinstitutiontypecode,
+                        source.educationinstitutiontypedescription,
+                        source.graduationyear,
+                        source.positionheld,
+                        source.degreeabbreviation,
+                        source.city,
+                        source.state,
+                        source.nationname)';
 
 ---------------------------------------------------------
 --------- 4. actions (inserts and updates) --------------
 ---------------------------------------------------------  
 
 
-merge_statement := ' merge into mid.provideraffiliation as target using 
+merge_statement := ' merge into mid.providereducation as target using 
                    ('||select_statement||') as source 
-                   on source.providertoaffiliationid = target.providertoaffiliationid
+                   on source.providertoeducationinstitutionid = target.providertoeducationinstitutionid
                    when matched then '||update_statement|| '
                    when not matched then '||insert_statement;
                    
@@ -108,7 +123,7 @@ merge_statement := ' merge into mid.provideraffiliation as target using
 ---------------------------------------------------------
 
 if (is_full) then
-    truncate table Mid.ProviderAffiliation;
+    truncate table Mid.ProviderEducation;
 end if; 
 execute immediate merge_statement ;
 
