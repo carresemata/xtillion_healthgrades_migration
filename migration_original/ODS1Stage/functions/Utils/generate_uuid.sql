588a52bcf96a2b7b0bee8c423d56aeb155119daf
Ricardo <rsanchez@xtillion.com>
2024-07-08 15:19:49 -0400

    Added comments to uuid udf

--- migration_original/ODS1Stage/functions/Utils/generate_uuid.sql
+++ migration_original/ODS1Stage/functions/Utils/generate_uuid.sql
@@ -5,8 +5,11 @@ AS
 $$
 CASE
     WHEN name IS NOT NULL THEN
+        -- Snowflake provides this function for v5 UUID generation. The resulting ids are deterministic and depend on the two arguments: namespace (a uuid string) and name (any string)
+        -- Passing the same arguments will always return the same UUID
         UUID_STRING('fe971b24-9572-4005-b22f-351e9c09274e', name)
     ELSE
+        -- Snowflake provides this function for v4 UUID generation. No arguments are passed and the resulting ids are random
         UUID_STRING()
 END
 $$
