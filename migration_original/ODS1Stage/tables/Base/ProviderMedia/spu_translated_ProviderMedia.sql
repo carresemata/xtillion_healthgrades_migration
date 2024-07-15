23bf46f4dda5a481351d73fa0c87f625e90cad3c
carresemata <carrese@rvohealth.com>
2024-07-15 10:09:01 -0400

    base.providermedia

--- migration_original/ODS1Stage/tables/Base/ProviderMedia/spu_translated_ProviderMedia.sql
+++ migration_original/ODS1Stage/tables/Base/ProviderMedia/spu_translated_ProviderMedia.sql
@@ -20,6 +20,7 @@ declare
     select_statement string; -- cte and select statement for the merge
     insert_statement string; -- insert statement for the merge
     update_statement string; -- update
+    delete_statement string;
     merge_statement string; -- merge statement to final table
     status string; -- status monitoring
     procedure_name varchar(50) default('sp_load_providermedia');
@@ -57,7 +58,6 @@ select
     json.media_MEDIALINK as MediaLink,
     ifnull(json.media_SOURCECODE, 'Profisee') as SourceCode,
     ifnull(json.media_LASTUPDATEDATE, current_timestamp()) as LastUpdateDate
-
 from cte_media as JSON
     join base.provider as P on p.providercode = json.providercode
     join base.mediatype as MT on mt.mediatypecode = json.media_MEDIATYPECODE 
@@ -76,7 +76,7 @@ insert_statement := '       insert
                                     SourceCode,
                                     LastUpdateDate)         
                              values 
-                                    (utils.generate_uuid(source.providerid || source.mediatypeid), 
+                                    (uuid_string(), 
                                     source.providerid,
                                     source.mediatypeid,
                                     source.mediadate,
@@ -101,11 +101,14 @@ update_statement := '
 ---------------------------------------------------------
 --------- 4. actions (inserts and updates) --------------
 ---------------------------------------------------------  
+-- Remove the providers and insert them again to avoid duplicate records
+delete_statement := 'delete from base.providermedia as target
+                        using ('|| select_statement ||') AS source
+                        where target.providerid = source.providerid;';
 
-merge_statement := ' merge into base.providermedia as target 
+merge_statement := 'merge into base.providermedia as target 
                     using (' || select_statement || ') as source
-                   on source.providerid = target.providerid and source.mediatypeid = target.mediatypeid
-                   when matched then ' || update_statement || '
+                   on source.providerid = target.providerid 
                    when not matched then '||insert_statement;
 
 ---------------------------------------------------------
@@ -115,6 +118,7 @@ merge_statement := ' merge into base.providermedia as target
 if (is_full) then
     truncate table Base.ProviderMedia;
 end if; 
+execute immediate delete_statement ;
 execute immediate merge_statement ;
 
 ---------------------------------------------------------
